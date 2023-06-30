d6b818bb5bbc9fff003a60b7d47e3c43a5b49dc2
JDavisWASD <jdavisWASD@gmail.com>
2023-06-30 16:45:55 -0700

    step 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const ballRadius = 10;
 const canvas = document.getElementById("gameBoard");
 const context = canvas.getContext("2d");
 
@@ -9,13 +10,20 @@ let y = canvas.height - 30;
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
     x += dx;
     y += dy;
 }
 
 function drawBall() {
     context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI *2);
+    context.arc(x, y, ballRadius, 0, Math.PI * 2);
     context.fillStyle = "#0095DD";
     context.fill();
     context.closePath();
