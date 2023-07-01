fa730a7992a661afc940651dbbdc9949d060f455
JDavisWASD <jdavisWASD@gmail.com>
2023-06-30 17:20:26 -0700

    step 6

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const ballRadius = 10;
+const brickColumnCount = 5;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetLeft = 30;
+const brickOffsetTop =30;
+const brickRowCount = 3;
+const brickWidth = 75;
+const bricks = [];
 const canvas = document.getElementById("gameBoard");
 const context = canvas.getContext("2d");
 const interval = setInterval(draw, 10);
@@ -7,20 +15,33 @@ const paddleWidth = 75;
 const paddleSpeed = 7;
 
 let horizBallSpeed = 2;
+let leftKeybind = "a";
+let leftPressed = false;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightKeybind = "d";
+let rightPressed = false;
 let vertBallSpeed = -2;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
-let paddleX = (canvas.width - paddleWidth) / 2;
-let rightKeybind = "d";
-let leftKeybind = "a";
-let rightPressed = false;
-let leftPressed = false;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+for (let col = 0; col < brickColumnCount; col++) {
+    bricks[col] = [];
+    for (let row = 0; row < brickRowCount; row++) {
+        bricks[col][row] = {
+            x: 0,
+            y: 0
+        };
+    }
+}
 
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
 
     if (x + horizBallSpeed > canvas.width - ballRadius || x + horizBallSpeed <
         ballRadius) {
@@ -59,6 +80,23 @@ function drawBall() {
     context.closePath();
 }
 
+function drawBricks() {
+    for (let col = 0; col < brickColumnCount; col++) {
+        for (let row = 0; row < brickRowCount; row++) {
+            const brickX = col * (brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = row * (brickHeight + brickPadding) + brickOffsetTop;
+
+            bricks[col][row].x = brickX;
+            bricks[col][row].y = brickY;
+            context.beginPath();
+            context.rect(brickX, brickY, brickWidth, brickHeight);
+            context.fillStyle = "#0095DD";
+            context.fill();
+            context.closePath();
+        }
+    }
+}
+
 function drawPaddle() {
     context.beginPath();
     context.rect(paddleX, canvas.height - paddleHeight, paddleWidth,
@@ -85,6 +123,3 @@ function keyUpHandler(event) {
         leftPressed = false;
     }
 }
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
