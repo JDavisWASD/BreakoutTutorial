d187a13a397cfefa425c6b09ed7d3a136a585994
JDavisWASD <jdavisWASD@gmail.com>
2023-06-30 17:04:13 -0700

    step 4

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const ballRadius = 10;
 const canvas = document.getElementById("gameBoard");
 const context = canvas.getContext("2d");
+const paddleHeight = 10;
+const paddleWidth = 75;
+const paddleSpeed = 7;
 
-let dx = 2;
-let dy = -2;
+let horizBallSpeed = 2;
+let vertBallSpeed = -2;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightKeybind = "d";
+let leftKeybind = "a";
+let rightPressed = false;
+let leftPressed = false;
 
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+    drawPaddle();
+
+    if (x + horizBallSpeed > canvas.width - ballRadius || x + horizBallSpeed <
+        ballRadius) {
+            horizBallSpeed = -horizBallSpeed;
     }
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-        dy = -dy;
+    if (y + vertBallSpeed > canvas.height - ballRadius || y + vertBallSpeed <
+        ballRadius) {
+            vertBallSpeed = -vertBallSpeed;
     }
 
-    x += dx;
-    y += dy;
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + paddleSpeed, canvas.width - paddleWidth);
+    }
+    else if (leftPressed) {
+        paddleX = Math.max(paddleX - paddleSpeed, 0);
+    }
+
+    x += horizBallSpeed;
+    y += vertBallSpeed;
 }
 
 function drawBall() {
@@ -29,4 +49,33 @@ function drawBall() {
     context.closePath();
 }
 
+function drawPaddle() {
+    context.beginPath();
+    context.rect(paddleX, canvas.height - paddleHeight, paddleWidth,
+        paddleHeight);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+}
+
+function keyDownHandler(event) {
+    if (event.key === rightKeybind) {
+        rightPressed = true;
+    }
+    else if (event.key === leftKeybind) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.key === rightKeybind) {
+        rightPressed = false;
+    }
+    else if (event.key === leftKeybind) {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
